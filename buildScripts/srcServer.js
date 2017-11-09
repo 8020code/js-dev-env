c9880f1a31084f24280e859a503b158d3d6cb97d
Hone <hone4292@gmail.com>
2017-11-09 14:37:00 +0200

    added mocking data for http calls

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -12,8 +12,18 @@ app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
-app.get('/', function(req, res) {
+
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+app.get('/users', function(req, res) {
+  res.json([
+    {"id":1, "firstName":"Bob","lastName":"Smith", "email":"bob@gmail.com"},
+    { "id": 2, "firstName": "Bob2", "lastName": "Smith2", "email": "bob2@gmail.com" },
+    {"id": 3, "firstName": "Bob3", "lastName": "Smith3", "email": "bob3@gmail.com" }
+  ]);
+
+  // res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(err){
--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Our first tets', () => {
 });
 
 describe('index.html', () => {
-  it('should say hello', (done) => { // for asyncronous test add par done
+  it('should have h1 that says Users', (done) => { // for asyncronous test add par done
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {// because of callback
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Hello World!");
+      expect(h1.innerHTML).to.equal("Users");
       done();// run done here
       window.close();
 
